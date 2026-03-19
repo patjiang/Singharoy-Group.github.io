cac2aa7884deff1cd47bc4596dadad2b8a38375d
Patrick Jiang <patjiang04@gmail.com>
2026-03-19 13:32:53 -0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,5 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://patjiang.github.io',
-  base: '/StructureSystemAI.github.io/',
+  base: '/Singharoy-Group.github.io/',
 });
